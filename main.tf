d93ac88f98d3dc71f9ce68a7be6362ba476dcd67
souhaomrani2 <163522102+souhaomrani2@users.noreply.github.com>
2024-05-16 23:49:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "proxmox" {
   pm_api_url         = "https://192.168.127.177:8006/api2/json"
-  pm_api_token_id    = "root@pam!souha2"  # Vérifiez votre ID de jeton API
-  pm_api_token_secret = "4368e63e-53e8-4e07-b25a-856cab14593e"  # Vérifiez votre secret de jeton API
+  pm_api_token_id    = "root@pam!souha2"
+  pm_api_token_secret = "4368e63e-53e8-4e07-b25a-856cab14593e"
   pm_tls_insecure    = true
 }
 
@@ -18,7 +18,8 @@ resource "proxmox_vm_qemu" "my_vm" {
   name        = "my-vm"
   target_node = "pve"
   clone       = "VM 1804"
-   agent = 1
+  agent       = 1
+
   provisioner "file" {
     source      = "cloud-init.yaml"
     destination = "/tmp/cloud-init.yaml"
