6c8b7ecd362108511199acb1b576cf515d58f524
souhaomrani2 <163522102+souhaomrani2@users.noreply.github.com>
2024-05-16 19:39:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
+      source = "telmate/proxmox"
       version = ">= 3.0.1"
     }
   }
 }
+
 provider "proxmox" {
   pm_api_url         = "https://192.168.127.177:8006/api2/json"
   pm_api_token_id    = "your-api-token-id"
