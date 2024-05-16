1b6d872263737ecc15193a2fd6a6ca40ef1faebb
souhaomrani2 <163522102+souhaomrani2@users.noreply.github.com>
2024-05-16 18:30:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,19 +10,19 @@ terraform {
 
 # Définition du provider Proxmox
 provider "proxmox" {
-  pm_api_url         = var.pm_api_url
-  pm_api_token_id    = var.pm_user
-  pm_api_token_secret = var.pm_password
-  pm_tls_insecure    = true
+  pm_api_url         = "https://192.168.127.177:8006/api2/json"
+  pm_api_token_id    = "root@pam!souha"
+  pm_api_token_secret = "f21d17d80-eda1-40e6-a9fa-6966c9475c59"
+  pm_tls_insecure    = true  # Utilisation de HTTPS (non sécurisé dans cet exemple)
 }
 
 # Définition de la machine virtuelle Proxmox avec Cloud-Init
 resource "proxmox_vm_qemu" "my_vm" {
-  name           = "my-vm"
-  target_node    = "pve"  # Utilisez le nom du nœud Proxmox cible
-  clone          = "VM 1804"  # Utilisez le nom du template à cloner défini dans les variables
-  memory         = 1024
-  cores          = 2
+  name        = "my-vm"
+  target_node = "pve"       # Utilisez le nom du nœud Proxmox cible
+  clone       = "VM 1804"   # Utilisez le nom du template à cloner défini dans les variables
+  memory      = 1024
+  cores       = 2
 
   # Configuration Cloud-Init
   provisioner "file" {
