9c6637320f9a9f53d85fe1871c4903d4e00fb15b
souhaomrani2 <163522102+souhaomrani2@users.noreply.github.com>
2024-05-16 19:10:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
 provider "proxmox" {
   pm_api_url         = "https://192.168.127.177:8006/api2/json"
   pm_api_token_id    = "root@pam!souha"
-  pm_api_token_secret = "f21d17d80-eda1-40e6-a9fa-6966c9475c59"
+  pm_api_token_secret = "21d17d80-eda1-40e6-a9fa-6966c9475c59 "
   pm_tls_insecure    = true  # Utilisation de HTTPS (non sécurisé dans cet exemple)
 }
 
